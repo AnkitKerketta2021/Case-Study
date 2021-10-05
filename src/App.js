7f713def6848bee9eabef2b46015ea81a9c5c648
AnkitKerketta2021 <ankitkerketta999.com>
2021-10-05 18:45:12 +0530

    created case-study

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Query from './components/Query';
+function App() {
+  return (
+    <div className="App">
+    <Query/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/yourQuestion/YourQuestion.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function yourQuestion() {
+    return (
+        <div>
+        <h1>Upload Your Questions</h1>
+            
+        </div>
+    )
+}
+
+export default yourQuestion
